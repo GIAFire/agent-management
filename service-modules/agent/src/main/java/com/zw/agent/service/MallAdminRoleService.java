5789893dd8109622f202c036210068f04948b227
w <weijbo@163.com>
2026-06-20 06:42:37 +0800

    删除多余实体

--- service-modules/agent/src/main/java/com/zw/agent/service/MallAdminRoleService.java
+++
@@ -1,7 +0,0 @@
-package com.zw.agent.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zw.agent.entity.MallAdminRole;
-
-public interface MallAdminRoleService extends IService<MallAdminRole> {
-}
--- service-modules/agent/src/main/java/com/zw/agent/service/MallAdminUserService.java
+++
@@ -1,7 +0,0 @@
-package com.zw.agent.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zw.agent.entity.MallAdminUser;
-
-public interface MallAdminUserService extends IService<MallAdminUser> {
-}
