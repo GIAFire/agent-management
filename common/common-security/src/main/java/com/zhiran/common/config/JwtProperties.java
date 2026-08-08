79492033f89f21a245aa4f1595c5eb9c9c4a2260
w <weijbo@163.com>
2026-08-08 21:44:40 +0800

    增强账号安全性

--- common/common-security/src/main/java/com/zhiran/common/config/JwtProperties.java
+++ common/common-security/src/main/java/com/zhiran/common/config/JwtProperties.java
@@ -1,36 +1,19 @@
 package com.zhiran.common.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
+@ToString(exclude = "secret")
 @ConfigurationProperties(prefix = "auth.jwt")
 public class JwtProperties {
     private String secret = "agentScopeJwtSecretKeyForLocalDevChangeMe20260621";
     private String issuer = "agent-scope";
     private long expireSeconds = 7 * 24 * 60 * 60;//7天过期
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public String getIssuer() {
-        return issuer;
-    }
-
-    public void setIssuer(String issuer) {
-        this.issuer = issuer;
-    }
-
-    public long getExpireSeconds() {
-        return expireSeconds;
-    }
-
-    public void setExpireSeconds(long expireSeconds) {
-        this.expireSeconds = expireSeconds;
-    }
 }
